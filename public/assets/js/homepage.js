8c8e41a13811041ff579eaf539f0ed052b5f07c0
Andy Kim <andyjinkim@gmail.com>
2016-01-04 17:45:40 -0800

    all links open in new windows

--- public/assets/js/homepage.js
+++ public/assets/js/homepage.js
@@ -1,19 +1,19 @@
 $("#p1").click(function(){
   $("#project").html('<span class="helper"></span><img style="width:275px;height:auto;position:relative" class="macbook" src="../assets/images/macbookp1.png">');
   $("#github").html('For detailed information on Othello, click on the Octocat, or click the Macbook for a larger image!');
-  $("#link").html('<a href="https://github.com/andyjinkim/andyjinkim/tree/master/assignments/othello_project_2/othello_project"><img style="width:100px;height:auto" src="../assets/images/octocat3.png" /></a>')
+  $("#link").html('<a href="https://github.com/andyjinkim/andyjinkim/tree/master/assignments/othello_project_2/othello_project" target="_blank"><img style="width:100px;height:auto" src="../assets/images/octocat3.png" /></a>')
 })
 
 $("#p2").click(function(){
   $("#project").html('<span class="helper"></span><img style="width:275px;height:auto;position:relative" class="macbook" src="../assets/images/macbookp2.png">');
   $("#github").html('For detailed information on CliMate, click on the Octocat, or click the Macbook for a larger image!');
-  $("#link").html('<a href="https://github.com/andyjinkim/SEAFOAM-s-CliMate-Project"><img style="width:100px;height:auto" src="../assets/images/octocat3.png" /></a>')
+  $("#link").html('<a href="https://github.com/andyjinkim/SEAFOAM-s-CliMate-Project" target="_blank"><img style="width:100px;height:auto" src="../assets/images/octocat3.png" /></a>')
 })
 
 $("#p3").click(function(){
   $("#project").html('<span class="helper"></span><img style="width:275px;height:auto;position:relative" class="macbook" src="../assets/images/macbookp3.png">');
   $("#github").html('For detailed information on TopOut, click on the Octocat, or click the Macbook for a larger image!');
-  $("#link").html('<a href="https://github.com/andyjinkim/topout-climbing-app"><img style="width:100px;height:auto" src="../assets/images/octocat3.png" /></a>')
+  $("#link").html('<a href="https://github.com/andyjinkim/topout-climbing-app" target="_blank"><img style="width:100px;height:auto" src="../assets/images/octocat3.png" /></a>')
 })
 //
 // $(".macbook").mouseover(function(){
